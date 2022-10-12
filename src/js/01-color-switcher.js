5cf0c69dce6af2cafd9b730ed51fedc24f7a0c03
Weselka <weselyna.shvets@gmail.com>
2022-10-12 23:38:51 +0300

    add task1
    add refs
    add click
    add HexColor
    add onClickStar if isActive
    add onClickStop

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+const refs = {
+  body: document.querySelector('body'),
+  btnStart: document.querySelector('[data-start]'),
+  btnStop: document.querySelector('[data-stop]'),
+};
 
+let idInterval = null;
+let isActive = false;
+
+refs.btnStart.addEventListener('click', onClickStartChangeColor);
+refs.btnStop.addEventListener('click', onClickStopChangeColor);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onClickStartChangeColor(event) {
+  if (isActive) {
+    return;
+  }
+    
+  isActive = true;
+    refs.body.style.backgroundColor = getRandomHexColor();
+    
+  idInterval = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+    console.log('change color');
+  }, 1000);
+}
+
+function onClickStopChangeColor(event) {
+  clearInterval(idInterval);
+  isActive = false;
+}
