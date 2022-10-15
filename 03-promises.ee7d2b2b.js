202f90f568122da4d6cabdfa6144f660e75c7331
Weselka <104644733+Weselka@users.noreply.github.com>
2022-10-15 20:21:43 +0000

    Deploying to gh-pages from @ Weselka/goit-js-hw-09@1855f5d3543c178aa86639bff69efbd818e56825 🚀

---
+++ 03-promises.ee7d2b2b.js
@@ -0,0 +1,2 @@
+var o;(o="slice",new Promise(((e,n)=>{const r=Math.random()>.5;setTimeout((()=>{r&&e(`✅ It's your ${o}`),n("❌ Sorry, out of products")}),1e3)}))).then((function(o){console.log("onMakeOrderSuccess"),console.log(o)})).catch((function(o){console.log("onMakeOrderError"),console.log(o)}));
+//# sourceMappingURL=03-promises.ee7d2b2b.js.map
