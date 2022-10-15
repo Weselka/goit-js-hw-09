bc24568bc8fcb1e833548a5900bc3000e628a4d6
Weselka <weselyna.shvets@gmail.com>
2022-10-15 20:43:08 +0300

    task2 update

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,15 +1,6 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
-// If using flatpickr in a framework, its recommended to pass the element directly
-flatpickr(element, {});
-
-// Otherwise, selectors are also supported
-flatpickr("#myID", {});
-
-// creates multiple instances
-flatpickr(".anotherSelector");
-
 const refs = {
   datetime: document.querySelector('#datetime-picker'),
   btnStart: document.querySelector('[data-start]'),
@@ -18,7 +9,8 @@ const refs = {
   dataMinutes: document.querySelector('[data-minutes]'),
   dataSeconds: document.querySelector('[data-seconds]'),
 };
-console.log(refs)
+
+refs.btnStart.disabled = true;
 
 const options = {
   enableTime: true,
@@ -26,39 +18,118 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    if (selectedDates[0] < Date.now()) {
+      window.alert('Please choose a date in the future');
+    } else {
+      refs.btnStart.disabled = false;
+    }
   },
 };
 
-refs.btnStart.addEventListener('click', onClickStartChangeColor);
+const datetimePicker = flatpickr('#datetime-picker', options);
 
-function onClickStartChangeColor(e) {
-  console.log('this will btnStart');
-}
+class TimerCountDown {
+  constructor({ onCount }) {
+    this.intervalId = null;
+    this.isActive = false;
+    this.onCount = onCount;
+  };
 
-function convertMs(ms) {
-  // Number of milliseconds per unit of time
+  start() {
+    if (this.isActive) {
+      return;
+    }
+    const startTime = datetimePicker.selectedDates[0].getTime();
+    this.isActive = true;
+
+    this.intervalId = setInterval(() => {
+      const currentTime = Date.now();
+      const deltaTime = startTime - currentTime;
+      const time = this.convertMs(deltaTime);
+
+      this.onCount(time);
+
+      if (deltaTime > 1000) {
+        clearInterval(this.intervalId);
+      }
+    }, 1000);
+  };
+  convertMs(ms) {
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-  // Remaining days
-  const days = Math.floor(ms / day);
-  // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  const days = this.addLeadingZero(Math.floor(ms / day));
+  const hours = this.addLeadingZero(Math.floor((ms % day) / hour));
+  const minutes = this.addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  const seconds = this.addLeadingZero(
+    Math.floor((((ms % day) % hour) % minute) / second)
+  );
 
   return { days, hours, minutes, seconds };
-}
-
-console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
-
-function addLeadingZero(value) {
-
+  };
+  
+  addLeadingZero(value) {
+  return String(value).padStart(2, '0');
 };
+};
+
+const timer = new TimerCountDown({
+  onCount: updateClockFace,
+});
+
+refs.btnStart.addEventListener('click', timer.start.bind(timer));
+
+function updateClockFace({ days, hours, minutes, seconds }) {
+  refs.dataDays.textContent = `${days}`;
+  refs.dataHours.textContent = `${hours}`;
+  refs.dataMinutes.textContent = `${minutes}`;
+  refs.dataSeconds.textContent = `${seconds}`;
+};
+
+// const timer = {
+//   intervalId: null,
+//   isActive: false,
+//   start() {
+//     if (this.isActive) {
+//       return;
+//     }
+//     const startTime = dataPickr.selectedDates[0].getTime();
+//     this.isActive = true;
+
+//     this.intervalId = setInterval(() => {
+//       const currentTime = Date.now();
+
+//       const deltaTime = startTime - currentTime;
+//       const time = convertMs(deltaTime);
+//       updateClockFace(time);
+//     }, 1000);
+//   },
+// };
+
+
+
+// function onClickStartTimer(e) {
+//   timer.start();
+// };
+
+// function convertMs(ms) {
+//   const second = 1000;
+//   const minute = second * 60;
+//   const hour = minute * 60;
+//   const day = hour * 24;
+
+//   const days = addLeadingZero(Math.floor(ms / day));
+//   const hours = addLeadingZero(Math.floor((ms % day) / hour));
+//   const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+//   const seconds = addLeadingZero(
+//     Math.floor((((ms % day) % hour) % minute) / second)
+//   );
+
+//   return { days, hours, minutes, seconds };
+// };
+
+// function addLeadingZero(value) {
+//   return String(value).padStart(2, '0');
+// };
