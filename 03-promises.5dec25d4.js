706bef7207ef1db9a87bc11116cb1f34190cb068
Weselka <104644733+Weselka@users.noreply.github.com>
2022-10-16 16:54:52 +0000

    Deploying to gh-pages from @ Weselka/goit-js-hw-09@bfcc77e9096e41f87b62d0bafcc22c61e5de8429 🚀

--- 03-promises.5dec25d4.js
+++
@@ -1,2 +0,0 @@
-new Promise((function(n,o){Math.random()>.5?n("Yes"):o("No")}));
-//# sourceMappingURL=03-promises.5dec25d4.js.map
--- 03-promises.d597ab84.js
+++
@@ -1,2 +0,0 @@
-new Promise(((e,o)=>{Math.random()>.5?e("Yes"):o("No")}));
-//# sourceMappingURL=03-promises.d597ab84.js.map
